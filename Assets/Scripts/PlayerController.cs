230a128769d5184c430a480cc72e65e32bfca747
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-07 02:17:53 +0530

    added movement to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,7 +22,9 @@ public class PlayerController : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal"); 
 		float moveVertical = Input.GetAxis ("Vertical");
 
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
+		rb.AddForce (movement);
 	}
 
 }
