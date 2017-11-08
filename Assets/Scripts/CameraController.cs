d61576f1f3ea5eb62debba8230f855a62fa49808
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-08 14:56:57 +0530

    added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		//offset = camera position - player position
+		offset = transform.position - player.transform.position;
+
+	}
+	
+	// LateUpdate runs every frame just like Update but it runs after all items 
+	// have been processed in the update.
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	} 
+}
